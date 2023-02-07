f446899ee0c72171549b613228ce19406c7a675f
alexandrejs777 <alexandrejatobasiqueira@gmail.com>
2023-02-06 22:44:24 -0300

    Operadores lógicos

--- js/principal.js
+++ js/principal.js
@@ -12,6 +12,26 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+var pesoEValido = true;
+var alturaEValida = true;
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (pesoEValido && alturaEValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
