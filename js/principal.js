3afe747592555b56f7d5bd02487300a68f75044b
alexandrejs777 <alexandrejatobasiqueira@gmail.com>
2023-02-08 23:00:14 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -2,36 +2,44 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Alura Nutrição";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++){
+    console.log(pacientes[i]);
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
 
 
-var pesoEValido = true;
-var alturaEValida = true;
+    var pesoEValido = true;
+    var alturaEValida = true;
 
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso inválido!");
-    pesoEValido = false;
-    tdImc.textContent = "Peso inválido!";
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!");
+        pesoEValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+
+    if (altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida!");
+        alturaEValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+
+    if (pesoEValido && alturaEValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida!");
-    alturaEValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
 
-if (pesoEValido && alturaEValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
 
 
 
