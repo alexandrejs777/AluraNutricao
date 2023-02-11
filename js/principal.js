c37b0f8afd3be08b610319b16a2f0cb4e84579be
alexandrejs777 <alexandrejatobasiqueira@gmail.com>
2023-02-11 14:44:24 -0300

    Adicionando pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -41,7 +41,7 @@ for (var i = 0; i < pacientes.length; i++){
     }
 }
 
-// Função nomeada
+// Exemplo de função nomeada
 /*
 titulo.addEventListener("click", mostraMenssagem);
 
@@ -51,7 +51,7 @@ function mostraMenssagem() {
 */
 
 
-// Função anônima
+// Exemplo de função anônima
 /*
 titulo.addEventListener ('click', function () {
     console.log("Olha só posso chamar uma função anônima!")
@@ -63,5 +63,40 @@ var botaoAdicionar = document.querySelector('#adicionar-paciente');
 
 botaoAdicionar.addEventListener('click', function () {
     event.preventDefault();
-    console.log("Botão adicionar paciente foi clicado!")
+    
+    // Capturar os dados do formulário
+    var formulario = document.querySelector('#form-adiciona');
+
+    var nome = formulario.nome.value;
+    var peso = formulario.peso.value;
+    var altura = formulario.altura.value;
+    var gordura = formulario.gordura.value;
+
+    // Criar as tabelas
+    var pacienteTr = document.createElement('tr');
+
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    // Colocar os TDs dentro do TR
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    // Colocar os elementos de tabela dentro da TB
+    var tabela = document.querySelector('#tabela-pacientes');
+
+    tabela.appendChild(pacienteTr);
+
+
+    // Inserir os dados do formulário dentro dos elementos criados
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
 });
