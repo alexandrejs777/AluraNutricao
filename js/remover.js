b04479e268a3c4b5ea17b5e60263539bdd625afe
alexandrejs777 <alexandrejatobasiqueira@gmail.com>
2023-02-19 11:12:42 -0300

    Delegando eventos

--- js/remover.js
+++ js/remover.js
@@ -1,3 +1,4 @@
+/*
 var pacientes =  document.querySelectorAll(".paciente");
 
 pacientes.forEach(function(paciente){
@@ -5,4 +6,16 @@ pacientes.forEach(function(paciente){
         console.log("Fui clicado com duplo click!");
         this.remove();
     })
+})
+*/
+
+var tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick', function(event){
+    /*
+    var alvoDoEvento = event.target;
+    var paiDoAlvo = alvoDoEvento.parentNode;
+    paiDoAlvo.remove();
+    */
+   event.target.parentNode.remove(); // A mesma função acima, porém mais simples.
 })
